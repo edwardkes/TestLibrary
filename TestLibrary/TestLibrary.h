fff28b408c976418e4d6558f22c8d48fe2f7851b
Edward Keselman <edwardk@menoramivt.co.il>
2023-09-05 14:42:34 +0300

    Initial Commit

---
+++ TestLibrary/TestLibrary.h
@@ -0,0 +1,18 @@
+//
+//  TestLibrary.h
+//  TestLibrary
+//
+//  Created by Edward Keselman on 05/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibrary.
+FOUNDATION_EXPORT double TestLibraryVersionNumber;
+
+//! Project version string for TestLibrary.
+FOUNDATION_EXPORT const unsigned char TestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrary/PublicHeader.h>
+
+
